ee45b9d02d229df81445cccca3fb497024f5d68b
IvoRosic <irosic20@foi.hr>
2022-11-18 22:40:23 +0100

    Implement phone number check

--- Software/app/src/main/java/hr/foi/rampu/stanarko/Registracija.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/Registracija.kt
@@ -11,32 +11,44 @@ class Registracija : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registracija)
-        val signUp = findViewById<Button>(R.id.btn_register);
+        val signUp = findViewById<Button>(R.id.btn_register)
         signUp.setOnClickListener{
-            RegisterUser();
+            RegisterUser()
         }
     }
 
     fun RegisterUser(){
-        val name = findViewById<EditText>(R.id.et_name).text.toString();
-        val surname = findViewById<EditText>(R.id.et_surname).text.toString();
-        val phoneNumber = findViewById<EditText>(R.id.et_phone_number).text.toString();
-        val mail = findViewById<EditText>(R.id.et_mail).text.toString();
-        val password = findViewById<EditText>(R.id.et_password).text.toString();
-        val confirmPassword = findViewById<EditText>(R.id.et_confirm_password).text.toString();
+        val name = findViewById<EditText>(R.id.et_name).text.toString()
+        val surname = findViewById<EditText>(R.id.et_surname).text.toString()
+        val phoneNumber = findViewById<EditText>(R.id.et_phone_number).text.toString()
+        val mail = findViewById<EditText>(R.id.et_mail).text.toString()
+        val password = findViewById<EditText>(R.id.et_password).text.toString()
+        val confirmPassword = findViewById<EditText>(R.id.et_confirm_password).text.toString()
 
-        if (name.isBlank()||surname.isBlank()||phoneNumber.isBlank()||mail.isBlank()||password.isBlank()||confirmPassword.isBlank())
-            Toast.makeText(baseContext,getString(R.string.blank_register_fields),Toast.LENGTH_SHORT).show();
+        if (!BlankCheck(name, surname, phoneNumber, mail, password, confirmPassword)) return
+        if(!PhoneNumberCheck(phoneNumber)) return
+        if(!PasswordCheck(password,confirmPassword)) return
+    }
 
-        if(!PasswordCheck(password,confirmPassword)) return;
+    fun BlankCheck(name: String, surname: String, phoneNumber: String, mail: String, password: String, confirmPassword: String): Boolean {
+        if (name.isBlank()||surname.isBlank()||phoneNumber.isBlank()||mail.isBlank()||password.isBlank()||confirmPassword.isBlank()){
+            Toast.makeText(baseContext,getString(R.string.blank_register_fields),Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
+
+    fun PhoneNumberCheck(phoneNumber: String): Boolean{
+        val pattern = Regex("^([\\d]{10,10})\$")
+        if (pattern.matches(phoneNumber))  return true
+        Toast.makeText(baseContext,getString(R.string.incorrect_phone_number),Toast.LENGTH_SHORT).show()
+        return false
     }
 
     fun PasswordCheck(password : String, confirmPassword : String): Boolean {
-        var check = true;
-        if(password!=confirmPassword){
-            Toast.makeText(baseContext,getString(R.string.incorrect_register_password),Toast.LENGTH_SHORT).show();
-            check = false;
-        }
-        return check;
+        if(password==confirmPassword)
+            return true
+        Toast.makeText(baseContext,getString(R.string.incorrect_register_password),Toast.LENGTH_SHORT).show()
+        return false
     }
 }
