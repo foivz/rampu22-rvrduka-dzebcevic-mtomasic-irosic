66a9edda23e2fd2d14e2f6dae4caf2b333717df7
IvoRosic <irosic20@foi.hr>
2022-11-18 22:21:43 +0100

    Implement blank field check

--- Software/app/src/main/java/hr/foi/rampu/stanarko/Registracija.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/Registracija.kt
@@ -8,22 +8,35 @@ import android.widget.Toast
 
 class Registracija : AppCompatActivity() {
 
-    val signUp = findViewById<Button>(R.id.btn_register);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registracija)
+        val signUp = findViewById<Button>(R.id.btn_register);
         signUp.setOnClickListener{
-            PasswordCheck();
+            RegisterUser();
         }
     }
 
-    fun PasswordCheck(){
-        var password = findViewById<EditText>(R.id.et_password);
-        var confirmPassword = findViewById<EditText>(R.id.et_confirm_password);
-        if(password.text===confirmPassword.text){
-            Toast.makeText(baseContext,"Password correct!",Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(baseContext,"Password incorrect!",Toast.LENGTH_SHORT).show();
+    fun RegisterUser(){
+        val name = findViewById<EditText>(R.id.et_name).text.toString();
+        val surname = findViewById<EditText>(R.id.et_surname).text.toString();
+        val phoneNumber = findViewById<EditText>(R.id.et_phone_number).text.toString();
+        val mail = findViewById<EditText>(R.id.et_mail).text.toString();
+        val password = findViewById<EditText>(R.id.et_password).text.toString();
+        val confirmPassword = findViewById<EditText>(R.id.et_confirm_password).text.toString();
+
+        if (name.isBlank()||surname.isBlank()||phoneNumber.isBlank()||mail.isBlank()||password.isBlank()||confirmPassword.isBlank())
+            Toast.makeText(baseContext,getString(R.string.blank_register_fields),Toast.LENGTH_SHORT).show();
+
+        if(!PasswordCheck(password,confirmPassword)) return;
+    }
+
+    fun PasswordCheck(password : String, confirmPassword : String): Boolean {
+        var check = true;
+        if(password!=confirmPassword){
+            Toast.makeText(baseContext,getString(R.string.incorrect_register_password),Toast.LENGTH_SHORT).show();
+            check = false;
         }
+        return check;
     }
 }
