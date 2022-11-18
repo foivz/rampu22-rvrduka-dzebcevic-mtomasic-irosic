92b4ab3b280bbec000f2f4dea05cdb126db981f2
Robert Vrđuka <rvrduka20@foi.hr>
2022-11-18 17:37:05 +0100

    Add recycler view adapter and viewHolder

--- Software/app/src/main/java/hr/foi/rampu/stanarko/MainActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/MainActivity.kt
@@ -2,7 +2,10 @@ package hr.foi.rampu.stanarko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import hr.foi.rampu.stanarko.adapters.FlatsAdapter
+import hr.foi.rampu.stanarko.helpers.MockDataLoader
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.rv_flat_list)
+        recyclerView.adapter = FlatsAdapter(MockDataLoader.getDemoFlat())
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
