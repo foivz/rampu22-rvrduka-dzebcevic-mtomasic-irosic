93d60c59569a1b36d76f8141f7c57b17a0100653
IvoRosic <irosic20@foi.hr>
2022-11-18 21:39:47 +0100

    Implement password check

--- Software/app/src/main/java/hr/foi/rampu/stanarko/Registracija.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/Registracija.kt
@@ -2,10 +2,28 @@ package hr.foi.rampu.stanarko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class Registracija : AppCompatActivity() {
+
+    val signUp = findViewById<Button>(R.id.btn_register);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registracija)
+        signUp.setOnClickListener{
+            PasswordCheck();
+        }
+    }
+
+    fun PasswordCheck(){
+        var password = findViewById<EditText>(R.id.et_password);
+        var confirmPassword = findViewById<EditText>(R.id.et_confirm_password);
+        if(password.text===confirmPassword.text){
+            Toast.makeText(baseContext,"Password correct!",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(baseContext,"Password incorrect!",Toast.LENGTH_SHORT).show();
+        }
     }
 }
