c59e1d56fbca7b39077e2d31c2a374a969a0a9b7
IvoRosic <irosic20@foi.hr>
2022-12-18 18:59:24 +0100

    Added firebase database

--- Software/app/src/main/java/hr/foi/rampu/stanarko/MainActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/MainActivity.kt
@@ -2,6 +2,9 @@ package hr.foi.rampu.stanarko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
