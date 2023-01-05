934c5b6dc849d4e3d3a89f980cd43d576e75b2f4
Matija Tomašić <mtomasic20@student.foi.hr>
2023-01-05 11:52:42 +0100

    Removed old and added new layouts for generating an displaying rent bills

--- Software/app/src/main/java/hr/foi/rampu/stanarko/RentManagerActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/RentManagerActivity.kt
@@ -3,9 +3,9 @@ package hr.foi.rampu.stanarko
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RentDetailActivity : AppCompatActivity() {
+class RentManagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rent_detail)
+        setContentView(R.layout.activity_rent_manager)
     }
 }
