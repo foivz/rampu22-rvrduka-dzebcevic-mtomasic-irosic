26dc6eb79155ff4aa0c8da766c3dc5a12ba76c39
Matija Tomašić <mtomasic20@student.foi.hr>
2023-01-07 17:35:39 +0100

    Database and entity classes correction

--- Software/app/src/main/java/hr/foi/rampu/stanarko/entities/Flat.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/entities/Flat.kt
@@ -4,7 +4,10 @@ data class Flat(
     val id : Int,
     val address: String,
     val city: String,
-    val owner: Owner,
+    val owner: Owner?,
     val occupied: Boolean,
+    val amount: Double,
     val postalCode: Int
-)
+){
+    constructor() : this(0, "", "", null, false,0.00,0)
+}
