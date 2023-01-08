7eb3f29c13ee42f3508ca435d9e7628c37daa9f7
IvoRosic <irosic20@foi.hr>
2023-01-08 18:11:57 +0100

    Created folder F01_Registracija for RegistracijaActivity classes #11 folder will contain all classes that are relevant just to RegistracijaActivity.kt file. The classes will be organized in this folder to improve the structure and organization of codebase.

--- Software/app/src/main/java/hr/foi/rampu/stanarko/F01_Registracija/Registracija.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/F01_Registracija/Registracija.kt
@@ -1,10 +1,11 @@
-package hr.foi.rampu.stanarko
+package hr.foi.rampu.stanarko.F01_Registracija
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import hr.foi.rampu.stanarko.R
 
 class Registracija : AppCompatActivity() {
 
