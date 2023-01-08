adff0a63391d6a3105fe0b249042b924f264bf46
IvoRosic <irosic20@foi.hr>
2023-01-08 23:18:25 +0100

    Implemented createTenant() method in 'TenantsDAO' #11 createTenant() method takes 'Tenant' data from 'Registracija' and adds that data to database.

--- Software/app/src/main/java/hr/foi/rampu/stanarko/database/TenantsDAO.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/database/TenantsDAO.kt
@@ -1,8 +1,11 @@
 package hr.foi.rampu.stanarko.database
 
+import android.content.Context
+import android.widget.Toast
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
+import hr.foi.rampu.stanarko.entities.Tenant
 
 class TenantsDAO {
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
@@ -15,5 +18,13 @@ class TenantsDAO {
 
     fun getAllTenants(): Task<QuerySnapshot> {
         return db.collection("tenants").get()
+    }
+
+    fun createTenant(tenant: Tenant, context: Context){
+        db.collection("tenants").add(tenant).addOnSuccessListener {
+            Toast.makeText(context, "Data inserted",Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener { e ->
+            Toast.makeText(context,"Error:${e.message}",Toast.LENGTH_SHORT).show()
+        }
     }
 }
