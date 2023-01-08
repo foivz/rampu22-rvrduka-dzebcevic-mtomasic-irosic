2570c178f4fc9507af3b0ff87496612c2a468f03
Matija Tomašić <mtomasic20@student.foi.hr>
2023-01-08 17:05:07 +0100

    Started working on dialog that will show more info about single rent

--- Software/app/src/main/java/hr/foi/rampu/stanarko/adapters/RentsAdapter.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/adapters/RentsAdapter.kt
@@ -1,20 +1,28 @@
 package hr.foi.rampu.stanarko.adapters
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.app.ActivityCompat.recreate
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import hr.foi.rampu.stanarko.R
+import hr.foi.rampu.stanarko.RentManagerActivity
+import hr.foi.rampu.stanarko.database.RentsDAO
 import hr.foi.rampu.stanarko.entities.Rent
 import java.text.DateFormatSymbols
 import java.text.SimpleDateFormat
 import java.util.*
 
-class RentsAdapter(private val rentLists: List<Rent>) : RecyclerView.Adapter<RentsAdapter.RentViewHolder>() {
+class RentsAdapter(private val rentLists: MutableList<Rent>,) : RecyclerView.Adapter<RentsAdapter.RentViewHolder>() {
     inner class RentViewHolder(view: View) : RecyclerView.ViewHolder(view)  {
+        private val rentsDAO = RentsDAO();
+
         private val sdf: SimpleDateFormat = SimpleDateFormat("dd.MM.yyyy. HH:mm", Locale.ENGLISH)
 
         private val rentPersonName : TextView
@@ -32,6 +40,26 @@ class RentsAdapter(private val rentLists: List<Rent>) : RecyclerView.Adapter<Ren
             rentDueYear = view.findViewById(R.id.tv_rent_due_year)
             rentDueMonthYear = view.findViewById(R.id.ll_month_year)
             rentAmount = view.findViewById(R.id.tv_rent_amount)
+            view.setOnClickListener {
+
+                AlertDialog.Builder(view.context)
+                    .setTitle(rentPersonName.text)
+                    .setNeutralButton("Plati račun") { _, _ ->
+                        val paidRent = rentLists[adapterPosition]
+                        rentsDAO.payRentByDocumentID("id", paidRent.id)
+                        removeRentFromList()
+                    }
+                    .setNegativeButton("Odustani") { dialog, _ ->
+                        dialog.cancel();
+                    }
+                    .show()
+                return@setOnClickListener
+            }
+        }
+
+        private fun removeRentFromList() {
+            rentLists.removeAt(adapterPosition)
+            notifyItemRemoved(adapterPosition)
         }
 
         @SuppressLint("SetTextI18n")
