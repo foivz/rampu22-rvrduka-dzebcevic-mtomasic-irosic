be23f70b6781e775386e89d323f185932053601f
IvoRosic <irosic20@foi.hr>
2023-01-08 22:23:44 +0100

    Implement tenant registration and correct warnings #11 Tenant registration implemented using built-in Firebase Authentication function. On successful registration Toast messages pops up with "Registracija uspiješna" message and on failed registration pops up "Registracija neuspješna" message. Corrected warnings by adding private modifiers to functions that are used within 'Registracija' class

--- Software/app/src/main/java/hr/foi/rampu/stanarko/F01_Registracija/Registracija.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/F01_Registracija/Registracija.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import hr.foi.rampu.stanarko.R
 
 class Registracija : AppCompatActivity() {
@@ -14,11 +15,11 @@ class Registracija : AppCompatActivity() {
         setContentView(R.layout.activity_registracija)
         val signUp = findViewById<Button>(R.id.btn_register)
         signUp.setOnClickListener{
-            RegisterUser()
+            registerUser()
         }
     }
 
-    fun RegisterUser(){
+    private fun registerUser(){
         val name = findViewById<EditText>(R.id.et_name).text.toString()
         val surname = findViewById<EditText>(R.id.et_surname).text.toString()
         val phoneNumber = findViewById<EditText>(R.id.et_phone_number).text.toString()
@@ -26,13 +27,24 @@ class Registracija : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.et_password).text.toString()
         val confirmPassword = findViewById<EditText>(R.id.et_confirm_password).text.toString()
 
-        if (!BlankCheck(name, surname, phoneNumber, mail, password, confirmPassword)) return
-        if(!PhoneNumberCheck(phoneNumber)) return
-        if(!MailCheck(mail)) return
-        if(!PasswordCheck(password,confirmPassword)) return
+        if (!blankCheck(name, surname, phoneNumber, mail, password, confirmPassword)) return
+        if(!phoneNumberCheck(phoneNumber)) return
+        if(!mailCheck(mail)) return
+        if(!passwordCheck(password,confirmPassword)) return
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(mail, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Registration successful
+                    Toast.makeText(this,"Registracija uspiješna", Toast.LENGTH_SHORT).show()
+                } else {
+                    // Registration failed
+                    Toast.makeText(this,"Registracija neuspješna", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
-    fun BlankCheck(name: String, surname: String, phoneNumber: String, mail: String, password: String, confirmPassword: String): Boolean {
+    private fun blankCheck(name: String, surname: String, phoneNumber: String, mail: String, password: String, confirmPassword: String): Boolean {
         if (name.isBlank()||surname.isBlank()||phoneNumber.isBlank()||mail.isBlank()||password.isBlank()||confirmPassword.isBlank()){
             Toast.makeText(baseContext,getString(R.string.blank_register_fields),Toast.LENGTH_SHORT).show()
             return false
@@ -40,24 +52,24 @@ class Registracija : AppCompatActivity() {
         return true
     }
 
-    fun PhoneNumberCheck(phoneNumber: String): Boolean{
+    private fun phoneNumberCheck(phoneNumber: String): Boolean{
         val pattern = Regex("^([\\d]{10,10})\$")
         if (pattern.matches(phoneNumber))  return true
-        Toast.makeText(baseContext,getString(R.string.incorrect_phone_number),Toast.LENGTH_SHORT).show()
+        Toast.makeText(baseContext,getString(R.string.incorrect_phone_number), Toast.LENGTH_SHORT).show()
         return false
     }
 
-    fun MailCheck(mail: String): Boolean {
+    private fun mailCheck(mail: String): Boolean {
         val pattern = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
         if (pattern.matches(mail)) return true
-        Toast.makeText(baseContext,getString(R.string.incorrect_mail_register),Toast.LENGTH_SHORT).show()
+        Toast.makeText(baseContext,getString(R.string.incorrect_mail_register), Toast.LENGTH_SHORT).show()
         return false
     }
 
-    fun PasswordCheck(password : String, confirmPassword : String): Boolean {
+    private fun passwordCheck(password : String, confirmPassword : String): Boolean {
         if(password==confirmPassword)
             return true
-        Toast.makeText(baseContext,getString(R.string.incorrect_register_password),Toast.LENGTH_SHORT).show()
+        Toast.makeText(baseContext,getString(R.string.incorrect_register_password), Toast.LENGTH_SHORT).show()
         return false
     }
 }
