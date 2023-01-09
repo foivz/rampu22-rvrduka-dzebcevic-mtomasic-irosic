d8f0a112fb72017c5a5a5ffd4641a23c1e7860d0
IvoRosic <irosic20@foi.hr>
2023-01-09 04:19:16 +0100

    Added 'Prijava' activity for user login #6 Generated empty 'Prijava' activity for use in login process

---
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/F02_Prijava/Prijava.kt
@@ -0,0 +1,12 @@
+package hr.foi.rampu.stanarko.F02_Prijava
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.foi.rampu.stanarko.R
+
+class Prijava : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prijava)
+    }
+}
