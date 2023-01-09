06bb419b15b7620c4bff4174851d618db1964059
IvoRosic <irosic20@foi.hr>
2023-01-09 11:30:31 +0100

    Implemented login with Firebase Authentication and placed 'Prijava' activity as start-up activity #6 Implemented login using built-in Firebasae authentication method signInWithEmailAndPassword() and placed activity 'Prijava' as start-up activity

--- Software/app/src/main/java/hr/foi/rampu/stanarko/F02_Prijava/Prijava.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/F02_Prijava/Prijava.kt
@@ -2,11 +2,30 @@ package hr.foi.rampu.stanarko.F02_Prijava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import hr.foi.rampu.stanarko.R
 
 class Prijava : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prijava)
+        val loginButton = findViewById<Button>(R.id.btn_login)
+        loginButton.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        val mail = findViewById<EditText>(R.id.et_mail).text.toString()
+        val password = findViewById<EditText>(R.id.et_password).text.toString()
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(mail,password).addOnSuccessListener {
+            Toast.makeText(this,"Successful login",Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this,"Invalid login",Toast.LENGTH_SHORT).show()
+        }
     }
 }
