062259a25ff6e88abc23116c07aa162b5a61a6ee
IvoRosic <irosic20@foi.hr>
2023-01-09 14:32:11 +0100

    Added spannable TextView to 'Prijava' and redirected spannable TextView in 'Registracija' #6 Added spannable TextView which on click sends user to 'Registracija'. Redirected spannable in 'Registracija' from 'MainActivity' to 'Prijava'

--- Software/app/src/main/java/hr/foi/rampu/stanarko/F02_Prijava/Prijava.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/F02_Prijava/Prijava.kt
@@ -1,11 +1,18 @@
 package hr.foi.rampu.stanarko.F02_Prijava
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import hr.foi.rampu.stanarko.F01_Registracija.Registracija
 import hr.foi.rampu.stanarko.R
 
 class Prijava : AppCompatActivity() {
@@ -16,6 +23,17 @@ class Prijava : AppCompatActivity() {
         loginButton.setOnClickListener {
             loginUser()
         }
+        val spannable = SpannableString("Don't have an account? Register")
+        val span = object : ClickableSpan(){
+            override fun onClick(widget: View) {
+                val intent = Intent(this@Prijava, Registracija::class.java)
+                startActivity(intent)
+            }
+        }
+        spannable.setSpan(span,23,31,0)
+        val register = findViewById<TextView>(R.id.register_prompt)
+        register.text = spannable
+        register.movementMethod = LinkMovementMethod.getInstance()
     }
 
     private fun loginUser() {
