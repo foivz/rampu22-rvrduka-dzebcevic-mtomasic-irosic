ab285f4e20f20931dd4fe11afe51aaa6b9d8e213
IvoRosic <irosic20@foi.hr>
2023-01-22 18:54:57 +0100

    Implemented DrawerLayout for 'activity_owner_drawer.xml' #22 Added DrawerLayout variable 'drawerLayout' and inflated 'activity_owner_drawer.xml' as parent and added FrameLayout variable 'container' that holds activity that will hold the content that will be shown

--- Software/app/src/main/java/hr/foi/rampu/stanarko/NavigationDrawer/OwnerDrawerActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/NavigationDrawer/OwnerDrawerActivity.kt
@@ -2,11 +2,17 @@ package hr.foi.rampu.stanarko.NavigationDrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.drawerlayout.widget.DrawerLayout
 import hr.foi.rampu.stanarko.R
 
 class OwnerDrawerActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_owner_drawer)
+    lateinit var drawerLayout: DrawerLayout
+    override fun setContentView(view: View?) {
+        drawerLayout = layoutInflater.inflate(R.layout.activity_owner_drawer,null) as DrawerLayout
+        var container: FrameLayout = drawerLayout.findViewById(R.id.activity_container)
+        container.addView(view)
+        super.setContentView(drawerLayout)
     }
 }
