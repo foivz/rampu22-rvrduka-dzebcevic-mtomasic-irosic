f57fd8d3c36384f321abea7dbd4799048555ab5c
IvoRosic <irosic20@foi.hr>
2023-01-22 14:41:00 +0100

    Created drawer activites for different roles #22 Created activites 'OwnerDrawerActivity.kt' for 'Owner' role and 'TenantDrawerActivity.kt' for 'Tenant' role

---
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/NavigationDrawer/OwnerDrawerActivity.kt
@@ -0,0 +1,12 @@
+package hr.foi.rampu.stanarko.NavigationDrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.foi.rampu.stanarko.R
+
+class OwnerDrawerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_owner_drawer)
+    }
+}
---
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/NavigationDrawer/TenantDrawerActivity.kt
@@ -0,0 +1,12 @@
+package hr.foi.rampu.stanarko.NavigationDrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.foi.rampu.stanarko.R
+
+class TenantDrawerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tenant_drawer)
+    }
+}
