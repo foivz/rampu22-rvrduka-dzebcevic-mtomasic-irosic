8152d692deb3448c745a9268ded5682ce622dd9d
Matija Tomašić <mtomasic20@student.foi.hr>
2023-01-26 19:28:57 +0100

    Added new methods to RentsDAO class and TenantsDAO classes #9

--- Software/app/src/main/java/hr/foi/rampu/stanarko/database/TenantsDAO.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/database/TenantsDAO.kt
@@ -1,6 +1,7 @@
 package hr.foi.rampu.stanarko.database
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FieldValue
@@ -8,9 +9,17 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.model.mutation.Precondition.exists
 import hr.foi.rampu.stanarko.entities.Tenant
+import kotlinx.coroutines.tasks.await
 
 class TenantsDAO {
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    suspend fun isUserTenant(userMail: String) : Boolean{
+        var tenants = db.collection("tenants")
+            .whereEqualTo("mail", userMail)
+            .get().await()
+        return tenants.size() > 0
+    }
 
     fun getTenantByID(tenantID : Int): Task<QuerySnapshot> {
         return db.collection("flats")
