ac793ba2a272e398753801bedded941283de79b4
IvoRosic <irosic20@foi.hr>
2023-01-26 19:28:36 +0100

    Implemented user session check in 'Prijava.kt' #6 Implemented method that will check if the user is already logged in, if the user is logged in it will redirect him to appropriate activity. Also added blankCheck() method that will check if the user has filled login information, if not it will write a toast message

--- Software/app/src/main/java/hr/foi/rampu/stanarko/F02_Prijava/Prijava.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/F02_Prijava/Prijava.kt
@@ -1,5 +1,6 @@
 package hr.foi.rampu.stanarko.F02_Prijava
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -22,6 +23,7 @@ import hr.foi.rampu.stanarko.TenantActivity
 class Prijava : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sessionCheck()
         setContentView(R.layout.activity_prijava)
         val loginButton = findViewById<Button>(R.id.btn_login)
         loginButton.setOnClickListener {
@@ -30,9 +32,38 @@ class Prijava : AppCompatActivity() {
         spannableString()
     }
 
+    private fun sessionCheck() {
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user!=null){
+            val userMail = FirebaseAuth.getInstance().currentUser!!.email
+            val ownersCollection = FirebaseFirestore.getInstance().collection("owners")
+            ownersCollection.whereEqualTo("mail",userMail).get().addOnSuccessListener { document ->
+                if(!document.isEmpty){
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    intent.putExtra("Email",userMail)
+                    startActivity(intent)
+                }
+                val tenantsCollection = FirebaseFirestore.getInstance().collection("tenants")
+                tenantsCollection.whereEqualTo("mail",userMail).get().addOnSuccessListener { document ->
+                    if(!document.isEmpty){
+                        val intent = Intent(this,TenantActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        intent.putExtra("Email",userMail)
+                        startActivity(intent)
+                    }
+                }
+            }
+        } else {
+            return
+        }
+    }
+
     private fun loginUser() {
         val mail = findViewById<EditText>(R.id.et_mail).text.toString()
         val password = findViewById<EditText>(R.id.et_password).text.toString()
+
+        if (!blankCheck(baseContext, mail, password)) return
 
         FirebaseAuth.getInstance().signInWithEmailAndPassword(mail,password).addOnSuccessListener {
             val userMail = FirebaseAuth.getInstance().currentUser!!.email
@@ -51,13 +82,20 @@ class Prijava : AppCompatActivity() {
                         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                         intent.putExtra("Email",mail)
                         startActivity(intent)
-                        Toast.makeText(this,"We have a tenant",Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }.addOnFailureListener {
             Toast.makeText(this,"Invalid login",Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun blankCheck(context: Context, mail: String, password: String): Boolean {
+        if (mail.isBlank()||password.isBlank()){
+            Toast.makeText(context,context.getString(R.string.blank_register_fields),Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
     }
 
     private fun spannableString(){
