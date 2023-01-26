b88a2e6a06b5796d45d04c695567ba34c190ea9d
IvoRosic <irosic20@foi.hr>
2023-01-26 01:31:32 +0100

    Implemented toolbar and navigation view listener for 'activity_owner_drawer.xml'  #22 Added Toolbar variable 'toolbar' and set it as the support action bar and added NavigationView  variable 'navView' and set a listener to it for handling item selection.

--- Software/app/src/main/java/hr/foi/rampu/stanarko/NavigationDrawer/TenantDrawerActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/NavigationDrawer/TenantDrawerActivity.kt
@@ -2,6 +2,7 @@ package hr.foi.rampu.stanarko.NavigationDrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.FrameLayout
 import androidx.drawerlayout.widget.DrawerLayout
@@ -9,7 +10,7 @@ import hr.foi.rampu.stanarko.R
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.navigation.NavigationView
 
-class TenantDrawerActivity : AppCompatActivity() {
+class TenantDrawerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var drawerLayout: DrawerLayout
     override fun setContentView(view: View?) {
         drawerLayout = layoutInflater.inflate(R.layout.activity_tenant_drawer, null) as DrawerLayout
@@ -17,5 +18,14 @@ class TenantDrawerActivity : AppCompatActivity() {
         container.addView(view)
         super.setContentView(drawerLayout)
 
+        var toolbar: Toolbar = drawerLayout.findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        var navView: NavigationView = drawerLayout.findViewById(R.id.nav_view)
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
