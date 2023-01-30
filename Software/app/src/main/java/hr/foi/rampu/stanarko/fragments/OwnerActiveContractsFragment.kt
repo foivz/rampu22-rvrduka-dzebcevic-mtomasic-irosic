aa3b92469b62930cc0a2738a927eb34c05f0d72b
IvoRosic <irosic20@foi.hr>
2023-01-30 04:51:19 +0100

    Removed FloatingButton from 'OwnerExpiredContractsFragment.kt' and initialized FloatingButton variable into 'OwnerActiveContractsFragment.kt' #14 Removed FloatingButton from 'OwnerExpiredContractsFragment.kt' and initialized FloatingButton variable into 'OwnerActiveContractsFragment.kt' that will be used for creating new contract

--- Software/app/src/main/java/hr/foi/rampu/stanarko/fragments/OwnerActiveContractsFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/fragments/OwnerActiveContractsFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import hr.foi.rampu.stanarko.R
 
 class OwnerActiveContractsFragment : Fragment() {
+
+    private lateinit var btnCreateContract: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
