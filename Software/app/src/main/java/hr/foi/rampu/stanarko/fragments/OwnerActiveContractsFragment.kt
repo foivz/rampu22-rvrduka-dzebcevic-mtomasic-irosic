efc26da9e4d3a1a9e62e7e4f6e3667c3010bf6c5
IvoRosic <irosic20@foi.hr>
2023-01-30 17:53:38 +0100

    Added recycleView to 'OwnerActiveFragment.kt' and created 'ContractRecycleViewAdapter.kt' adapter #14 Added recycleView to 'OwnerActiveFragment.kt' and created 'ContractRecycleViewAdapter.kt' adapter for managing recycleView items

--- Software/app/src/main/java/hr/foi/rampu/stanarko/fragments/OwnerActiveContractsFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/fragments/OwnerActiveContractsFragment.kt
@@ -9,16 +9,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CalendarView
 import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import hr.foi.rampu.stanarko.R
+import hr.foi.rampu.stanarko.adapters.ContractRecycleViewAdapter
 import hr.foi.rampu.stanarko.entities.Contract
 import hr.foi.rampu.stanarko.entities.Tenant
 import java.util.*
+import kotlin.collections.ArrayList
 
 class OwnerActiveContractsFragment : Fragment() {
 
     private lateinit var btnCreateContract: FloatingActionButton
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var contractList: ArrayList<Contract>
+    private var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +41,37 @@ class OwnerActiveContractsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_owner_active_contracts)
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+
+        contractList = arrayListOf()
+        db = FirebaseFirestore.getInstance()
+        val userMail = FirebaseAuth.getInstance().currentUser?.email
+
+        db.collection("contracts").whereEqualTo("tenant.flat.owner.mail",userMail).get()
+            .addOnSuccessListener {
+                if(!it.isEmpty){
+                    for(data in it.documents){
+                        val contract: Contract? =data.toObject(Contract::class.java)
+                        if (contract!=null){
+                            contractList.add(contract)
+                        }
+                    }
+                    val today = Calendar.getInstance().time
+                    val iterator = contractList.iterator()
+                    while (iterator.hasNext()) {
+                        val contract = iterator.next()
+                        if (contract.expiringDate!!.before(today)) {
+                            iterator.remove()
+                        }
+                    }
+                    recyclerView.adapter = ContractRecycleViewAdapter(contractList)
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, "Failed to do so", Toast.LENGTH_SHORT).show()
+            }
+
         btnCreateContract = view.findViewById(R.id.create_new_contract_floating_button)
         btnCreateContract.setOnClickListener {
             showDialog()
