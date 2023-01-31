8369bda9f4bd3140e581b784823c7bb03d94de3c
Robert Vrđuka <rvrduka20@foi.hr>
2023-01-31 19:27:47 +0100

    Add positive button to malfunction report #17

--- Software/app/src/main/java/hr/foi/rampu/stanarko/TenantActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/stanarko/TenantActivity.kt
@@ -3,7 +3,11 @@ package hr.foi.rampu.stanarko
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import hr.foi.rampu.stanarko.NavigationDrawer.TenantDrawerActivity
 import hr.foi.rampu.stanarko.databinding.ActivityTenantBinding
@@ -13,6 +17,7 @@ import kotlinx.coroutines.runBlocking
 class TenantActivity : TenantDrawerActivity() {
 
     lateinit var status: TextView
+    lateinit var malfunctionButton: Button
     lateinit var binding: ActivityTenantBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,8 @@ class TenantActivity : TenantDrawerActivity() {
         allocatedActivityTitle("TenantActivity")
         val userMail = FirebaseAuth.getInstance().currentUser?.email
         val user = runBlocking { MockDataLoader.getTenantByMail(userMail!!) }
+        malfunctionButton = findViewById(R.id.btn_malfunction)
+        malfunctionButton.visibility = View.GONE
         if (user.flat != null) {
             status = findViewById(R.id.tv_belongs_to_flat)
             status.text = buildString {
@@ -29,7 +36,26 @@ class TenantActivity : TenantDrawerActivity() {
                 append(user.flat.owner!!.name)
                 append(" ")
                 append(user.flat.owner.surname)
+                malfunctionButton.visibility = View.VISIBLE
+                malfunctionButton.setOnClickListener {
+                    showMalfunction()
+                }
             }
         }
+    }
+
+    private fun showMalfunction() {
+        val newMalfunctionReportView =
+            LayoutInflater
+                .from(this)
+                .inflate(R.layout.malfunction_report, null)
+
+        AlertDialog.Builder(this)
+            .setView(newMalfunctionReportView)
+            .setTitle(getString(R.string.report_a_malfunction))
+            .setPositiveButton(getString(R.string.report)) { _,_ ->
+
+            }
+            .show()
     }
 }
